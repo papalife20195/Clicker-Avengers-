b6b8691f252f7ae57c1ee01cc36dd378983cc2f2
Viktor <worker.20195@gmail.com>
2023-05-13 17:42:11 +0300

    next1

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+document.body.style.backgroundImage = "url('https://catherineasquithgallery.com/uploads/posts/2021-02/1613542301_52-p-fon-dlya-prezentatsii-powerpoint-minimaliz-68.jpg')";
+
 const block = document.createElement('div');
-block.classList.add('my-block', 'red');
+block.classList.add('clik', 'red');
 const parent1 = document.querySelector('body');
-parent.appendChild(block);
+parent1.appendChild(block);
 
-const clikButton = document.createElement('button');
-clikButton.innerText = 'Щелк';
+const clickCounter = document.createElement('span');
+clickCounter.innerText = '0';
+block.appendChild(clickCounter);
 
-clikButton.addEventListener('click', function() {
-    alert('Вы нажали на кнопку!');
-  });
+const imgButton = document.createElement('img');
+imgButton.src = 'https://www.epicgames.com/fortnite/en-US/home';
+imgButton.addEventListener('click', function() {
+  count++;
+  clickCounter.innerText = count;
+});
 
-const parent = document.querySelector('.my-block');
-parent.appendChild(clikButton);
+block.appendChild(imgButton);
+
+let count = 0;
 
