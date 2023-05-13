509bbbaa12f7c78f1ad4b64734d9d5304c2e2553
Viktor <worker.20195@gmail.com>
2023-05-13 15:21:07 +0300

    123

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const block = document.createElement('div');
+block.classList.add('my-block', 'red');
+const parent1 = document.querySelector('body');
+parent.appendChild(block);
+
 const clikButton = document.createElement('button');
 clikButton.innerText = 'Щелк';
 
-button.addEventListener('click', function() {
+clikButton.addEventListener('click', function() {
     alert('Вы нажали на кнопку!');
   });
 
-const parent = document.querySelector('#parent-element');
-parent.appendChild(button);
+const parent = document.querySelector('.my-block');
+parent.appendChild(clikButton);
 
